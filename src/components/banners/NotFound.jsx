1595fe980f14390b573615b021d80ab2b3a94688
Nastya Igntchk <nastya.igntchk@gmail.com>
2024-05-22 12:39:29 +0300

    fix not found img style

--- src/components/banners/NotFound.jsx
+++ src/components/banners/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     return (
         <div className='flex flex-col justify-center items-center pt-[55px]'>
             <NotFoundIcon/>
-            <p className='font-semibold leading-[25px]'>We don&apos;t have such movies, look for another one</p>
+            <p className='font-semibold leading-[25px] text-center'>We don&apos;t have such movies, look for another one</p>
         </div>
     );
 };
